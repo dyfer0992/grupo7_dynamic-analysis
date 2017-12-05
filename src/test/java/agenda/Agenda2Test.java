e1cb226f68e5084c1d1e05f0d39bba0bedb3bbf6
Diego F. Trejos Espinosa <dyfer92@gmail.com>
2017-12-05 15:44:31 +0100

    Agregada la libreria JUNI y un test para que no de error al desplegar

--- src/test/java/agenda/Agenda2Test.java
+++ src/test/java/agenda/Agenda2Test.java
@@ -1,15 +1,38 @@
 package agenda;
 
-import junit.framework.TestCase;
+import junit.framework.*;
+import org.junit.*;
+import org.junit.Test;
+
+import agenda.Agenda2;
 
 public class Agenda2Test extends TestCase {
-
-	protected void setUp() throws Exception {
-		super.setUp();
+	
+	private Agenda2 agenda;
+	
+	@BeforeClass
+	public static void setUpClass() throws Exception { 
+		//Inicialización general de variables, escritura del log...
 	}
-
-	protected void tearDown() throws Exception {
-		super.tearDown();
+	@AfterClass
+	public static void tearDownClass() throws Exception { 
+		//Liberación de recursos, escritura en el log...
 	}
+	@Before
+	public void setUp() { 
+		//Inicialización de variables antes de cada Test
+		this.agenda = new Agenda2();
+	}
+	@After
+	public void tearDown() { 
+		//Tareas a realizar después de cada test
+	}
+	
+	@Test
+	public void testPrueba() {
+		assertTrue(true);
+	}
+	
+	
 
 }
