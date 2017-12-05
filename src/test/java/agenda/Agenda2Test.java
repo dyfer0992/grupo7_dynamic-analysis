8f94005a0bc365fd3e7bb12cdaf3c4dd5e77ed56
Diego F. Trejos Espinosa <dyfer92@gmail.com>
2017-12-05 15:32:21 +0100

    Creación de la Clase Test de Agenda2

---
+++ src/test/java/agenda/Agenda2Test.java
@@ -0,0 +1,15 @@
+package agenda;
+
+import junit.framework.TestCase;
+
+public class Agenda2Test extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}
