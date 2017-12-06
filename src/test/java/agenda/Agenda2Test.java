eecb5e28fd10ea4fe3a5b1b0be5d309c3607875e
Diego F. Trejos Espinosa <dyfer92@gmail.com>
2017-12-06 13:28:49 +0100

    Creación de SetUp en la clase de Test Creación de los dos primeros Test, con error en el segundo (testAnniadirEnAgendaNoVacia)

--- src/test/java/agenda/Agenda2Test.java
+++ src/test/java/agenda/Agenda2Test.java
@@ -4,27 +4,71 @@ import junit.framework.*;
 import org.junit.*;
 import org.junit.Test;
 
+import persona.Persona;
 import agenda.Agenda2;
 
 public class Agenda2Test extends TestCase {
 	
 	private Agenda2 agenda;
+	private Persona p1;
+	private Persona p2;
+	private Persona p3;
+	
+	private void inicializacionPersonas() {
+		agenda = new Agenda2();
+		p1 = new Persona();
+		p2 = new Persona();
+		p3 = new Persona();
+		
+		p1.ponerNombre("Diego");
+		p1.ponerApellidos("Trejos");
+		p1.ponerAnioNacim(1992);
+		p1.ponerCodPostal("28044");
+		p1.ponerDireccion("Gran Via");
+		p1.ponerPoblacion("Madrid");
+		p1.ponerProvincia("Madrid");
+		p1.ponerTelefono("65489756");
+		
+		p2.ponerNombre("Alvaro");
+		p2.ponerApellidos("Arranz");
+		p2.ponerAnioNacim(1994);
+		p2.ponerCodPostal("25458");
+		p2.ponerDireccion("Calle San Juan");
+		p2.ponerPoblacion("Guadalajara");
+		p2.ponerProvincia("Guadalajara");
+		p2.ponerTelefono("94521658");
+		
+		p3.ponerNombre("Antonio");
+		p3.ponerApellidos("Muñoz");
+		p3.ponerAnioNacim(1992);
+		p3.ponerCodPostal("28942");
+		p3.ponerDireccion("Calle Juanito");
+		p3.ponerPoblacion("Madrid");
+		p3.ponerProvincia("Madrid");
+		p3.ponerTelefono("61235489");
+	}
+	
 	
 	@Before
-	public void setUp() { 
-		//Inicialización de variables antes de cada Test
+	public void setUp() {
 		this.agenda = new Agenda2();
+		inicializacionPersonas();
 	}
 	@After
 	public void tearDown() { 
-		//Tareas a realizar después de cada test
+		
 	}
 	
 	@Test
-	public void testPrueba() {
-		assertTrue(true);
+	public void testAnniadirEnAgendaVacia() {
+		assertTrue(agenda.aniadirPersona(p1));
 	}
 	
+	@Test
+	public void testAnniadirEnAgendaNoVacia() {
+		agenda.aniadirPersona(p1);
+		assertTrue(agenda.aniadirPersona(p2));
+	}
 	
 
 }
